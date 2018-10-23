558403703af54708d3c95c565c0ed1c2733b224a
Krunal Shah <ktgshah@gmail.com>
2018-10-23 23:13:24 +0530

    Added hard and split transition functions

--- black.py
+++ black.py
@@ -1,43 +1,118 @@
-import random
+from Util import *
+import pdb
 
 
 class GameState:
-	"""
+    """
 
-	Blackjack Game state
+    Blackjack Game state
 
-	"""
+    """
 
-	def __init__(self, FirstCard, SecondCard, OppCard):
-		pass
+    def __init__(self, FirstCard, SecondCard, OppCard, FreshHand, AcePresent):
+        
+        # if you add other members add them to the hash and eq as well
+        self.OppCard = OppCard
+        self.FirstCard = FirstCard
+        self.SecondCard = SecondCard
+        self.FreshHand = FreshHand
+        self.AcePresent = AcePresent
 
 
-ActionList = ["Hit","Stand","Double","Split"]
+    def __hash__(self):
+            return hash((self.OppCard,
+                        self.FirstCard,
+                        self.SecondCard,
+                        self.FreshHand,
+                        self.AcePresent))
 
-# ValueDict = {}
+    def __eq__(self, other):
+        return (self.OppCard,
+                self.FirstCard,
+                self.SecondCard,
+                self.FreshHand,
+                self.AcePresent) == (other.OppCard,
+                                    other.FirstCard,
+                                    other.SecondCard,
+                                    other.FreshHand,
+                                    other.AcePresent)
+
+
 # maps states to float values
-
-#p = 4/13
-
 def InitializeValueDict():
-	pass
+    # TODO: Please finish this and the function GetStandValue
+    ValueDict = {}
+    return ValueDict
 
-def PerformValueIteration( ValueDict ):
-	for state in ValueDict.keys():
-		if state.type == "Hard":
-			PerformValueIterationHard(ValueDict, state, p)
-		elif state.type == "Soft":
-			PerformValueIterationSoft(ValueDict, state, p)
-		else:
-			PerformValueIterationPair(ValueDict, state, p)
+def PerformValueIteration( ValueDict, p):
+    for state in ValueDict.keys():
+        if state.SecondCard == -1:
+            PerformValueIterationHard(ValueDict, state, p)
+        elif state.FirstCard == state.SecondCard:
+            PerformValueIterationPair(ValueDict, state, p)
+        else:
+            PerformValueIterationSoft(ValueDict, state, p)
+
 def IsPolicySame( OldValueDict, NewValueDict):
-	pass
-def main():
-	ValueDict = InitializeValueDict()
-	p = 4/13
-	while True:
-		NewValueDict = ValueDict.copy()
-		PerformValueIteration(NewValueDict, p)
-		if IsPolicySame(ValueDict, NewValueDict):
-			break
-		ValueDict = NewValueDict
+    epsilon = 0.0001
+    for state in OldValueDict:
+        if (OldValueDict[state] - NewValueDict) > epsilon:
+            return False
+    return True
+
+
+def GetStandValue(state):
+    # handle ace
+    if state.OppCard == 11:
+        return 1
+    else:
+        return 0
+
+
+def PerformValueIterationHard(ValueDict, state, p):
+    ActionList = ["Hit","Stand","Double","Split"]
+
+    # for action Hit
+    HitValue = CalculateHitValue(ValueDict, state, p)
+
+    # for action Stand
+    StandValue = GetStandValue(state)
+    
+    # for action Double
+    DoubleValue = 0
+    if state.FreshHand:
+        DoubleValue = CalculateDoubleValue(ValueDict, state, p)
+
+    ValueDict[state] = max(HitValue, StandValue, DoubleValue)
+
+def PerformValueIterationPair(ValueDict, state, p):
+    # TODO: Handle Ace pair
+    ActionList = ["Hit","Stand","Double","Split"]
+
+    # for action Hit
+    HitValue = CalculateHitValue(ValueDict, state, p)
+    
+    # for action Stand
+    StandValue = GetStandValue(state)
+
+    # for action Double
+    DoubleValue = 0
+    if state.FreshHand:
+        DoubleValue = CalculateDoubleValue(ValueDict, state, p)
+    
+    # for action Split (TODO: Handle ace pair)
+    SplitValue = CalculateSplitValue(ValueDict, state, p)
+
+    ValueDict[state] = max(HitValue, StandValue, DoubleValue, SplitValue)
+
+
+if __name__ == '__main__':
+    ValueDict = InitializeValueDict()
+    p = 4/13
+    while True:
+        NewValueDict = ValueDict.copy()
+        PerformValueIteration(NewValueDict, p)
+        break
+        if IsPolicySame(ValueDict, NewValueDict):
+            break
+        ValueDict = NewValueDict
